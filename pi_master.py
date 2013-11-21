3fa4cbe65f2d0b9f115c38ac80e3da7c1dd24149
Carl Lipo <clipo@csulb.edu>
2013-11-20 16:44:20 -0800

    fixing basic code

--- pi_master.py
+++ pi_master.py
@@ -155,14 +155,21 @@ def main():
     # Listen on port 2947 (gpsd) of localhost
     session = gps.gps("localhost", "2947")
     session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+
+
+    latitude=0.0
+    longitude=0.0
+    oldNorthing=0.0
+    oldEasting=0.0
+    currentEasting=0.0
+    currentNorthing=0.0
+
     while True:
         try:
             report = session.next()
             # Wait for a 'TPV' report and display the current time
             # To see all report data, uncomment the line below
             # print report
-            latitude=0.0
-            longitude=0.0
             if report['class'] == 'TPV':
                 if hasattr(report, 'time'):
                     time = report.time
