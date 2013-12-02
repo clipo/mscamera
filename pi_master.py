6bd8d5b068eeb8b9188832b54bd667205eb9d1cc
Carl Lipo <clipo@csulb.edu>
2013-12-02 15:44:06 -0800

    fixing basic code

--- pi_master.py
+++ pi_master.py
@@ -195,7 +195,7 @@ def main():
             report = session.next()
             # Wait for a 'TPV' report and display the current time
             # To see all report data, uncomment the line below
-            # print report
+            print report
             if report['class'] == 'TPV':
                 if hasattr(report, 'time'):
                     cTime = report.time
