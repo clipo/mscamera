d76fd65e7c06b2cccca4acb4961ec25f9229e734
Carl Lipo <clipo@csulb.edu>
2013-12-11 14:45:45 -0800

    s

--- pi_camera.py
+++ pi_camera.py
@@ -49,6 +49,7 @@ while True:
     # basically do nothing but wait for the pin to go HIGH
     # do nothing.
     wait += 1
+    GPIO.setup(INPUT_PIN, GPIO.IN)
     if GPIO.input(INPUT_PIN) > 0:
         takePicture()
 
