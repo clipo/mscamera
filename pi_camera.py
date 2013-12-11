cfe5627300a7b8047edc7202cb3bbea420e98907
Carl Lipo <clipo@csulb.edu>
2013-12-11 14:28:34 -0800

    fixing basic code

--- pi_camera.py
+++ pi_camera.py
@@ -24,15 +24,22 @@ def takePicture():
     with picamera.PiCamera() as camera:
         camera.resolution = (1024, 768)
         camera.start_preview()
-        time.sleep(.1)
         camera.capture(filename, 'raw')
+
+def takeAPhoto():
+    ts=datetime.datetime.now()          # get time step
+    a= ts.strftime("%j%H%M%S")
+    filename = "P-"+a+".jpg"   # give image file time-stamped name
+    call(["python pi_takePhoto.py -awb sun --colfx 128:128 -o " + filename], shell=True) # call external program ro take a picture
+    ## now trigger the ftp of the image to the master computer. may need to encapsulate this so that it can be a subprocess
+
 
 GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)  # Set's GPIO pins to BOARD numbering
 INPUT_PIN = 23           # Pin 4
 GPIO.setup(INPUT_PIN, GPIO.IN)
 #GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, callback=takePicture)
-GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, callback=lambda x: takePicture())
+GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, callback=lambda x: takeAPhoto())
 # Wait for the input to go high, run the function when it does
 
 # Create a function to run when the input is high
