0ff018e831d15eb52b272b828fcfeea07ccfcee1
Carl Lipo <clipo@csulb.edu>
2013-12-11 14:11:39 -0800

    fixing basic code

--- pi_camera.py
+++ pi_camera.py
@@ -32,7 +32,7 @@ GPIO.setmode(GPIO.BOARD)  # Set's GPIO pins to BOARD numbering
 INPUT_PIN = 7           # Pin 4
 GPIO.setup(INPUT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 #GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, callback=takePicture)
-GPIO.add_event_detect(INPUT_PIN, GPIO.FALLING, callback=lambda x: takePicture(INPUT_PIN), bouncetime=2000)
+GPIO.add_event_detect(INPUT_PIN, GPIO.RISING, callback=lambda x: takePicture(INPUT_PIN), bouncetime=2000)
 # Wait for the input to go high, run the function when it does
 
 # Create a function to run when the input is high
@@ -47,7 +47,6 @@ while True:
 
     if GPIO.input(INPUT_PIN):
         print('Input was HIGH')
-        time.sleep(2)
 
 
 
